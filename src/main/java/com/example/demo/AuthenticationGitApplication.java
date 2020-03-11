0a87e5ea30555c6159a04b3d0b6f32a9606d251b
Prabhsimran <pswalia2u@gmail.com>
2020-03-11 16:33:12 +0530

    first_commit

---
+++ src/main/java/com/example/demo/AuthenticationGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationGitApplication.class, args);
+	}
+
+}
